f7629c66d5d4e9741ff01ba3cb99f0db161692be
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2012-12-02 23:52:39 +0100

    implemented init

--- src/sdd_game.erl
+++ src/sdd_game.erl
@@ -11,6 +11,41 @@
 
 -module(sdd_game).
 
+%% Records
+-record(state, {
+	board,
+	candidates,
+	complete = false
+}).
+
+%% Config
+-define(CANDIDATE_SOPHISTICATION, 3).
+
+%%% =================================================================================== %%%
+%%% GEN_SERVER CALLBACKS                                                                %%%
+%%% =================================================================================== %%%
+
+%% ------------------------------------------------------------------------------------- %%
+%% Initializes the history, and assigns the realizer function to it.
+%% Generates the intial sudoku board saves that as the first event
+
+init(_Opts) ->
+	EmptyHistory = sdd_history:new(fun realize_event/3),
+	InitialBoard = sdd_logic:generate_sudoku(),
+	InitialHistory = sdd_history:append(EmptyHistory, start, InitialBoard),
+	{ok, InitialHistory}.
+
+%%% =================================================================================== %%%
+%%% HISTORY CALLBACKS                                                                   %%%
+%%% =================================================================================== %%%
+
+%% ------------------------------------------------------------------------------------- %%
+%% Uses the initial sudoku board to create the initial state, including candidates
+
+realize_event(_EmptyState, start, InitialBoard) ->
+	InitialCandidates = sdd_logic:calculate_candidates(InitialBoard, ?CANDIDATE_SOPHISTICATION),
+	#state{board = InitialBoard, candidates = InitialCandidates}.
+
 %%% =================================================================================== %%%
 %%% TESTS                                                                               %%%
 %%% =================================================================================== %%%
@@ -20,7 +55,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 init_returnsHistoryWithInitialBoardAndCandidates_test() ->
-	History = init(no_options),
+	{ok, History} = init(no_options),
 	State = sdd_history:state(History),
 	?assertNot(State#state.board =:= undefined),
 	?assertNot(State#state.candidates =:= undefined),
