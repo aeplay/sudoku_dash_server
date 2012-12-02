d8e08756a559c422b528c5a40774c03180d399b2
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2012-12-02 23:02:05 +0100

    Added module description

--- src/sdd_game.erl
+++ src/sdd_game.erl
@@ -1,4 +1,17 @@
 %%% =================================================================================== %%%
+%%% Sudoku Dash: Game                                                                   %%%
+%%%                                                                                     %%%
+%%% Copyright 2012 Anselm Eickhoff                                                      %%%
+%%%                                                                                     %%%
+%%% This module manages one game of sudoku dash. Its job is to connect its history,     %%%
+%%% which is holding its state, to the participating players. It does so by             %%%
+%%% registering players as listeners of the history and offers actions that players     %%%
+%%% can take, which will generate new history events.                                   %%%
+%%% =================================================================================== %%%
+
+-module(sdd_game).
+
+%%% =================================================================================== %%%
 %%% TESTS                                                                               %%%
 %%% =================================================================================== %%%
 
