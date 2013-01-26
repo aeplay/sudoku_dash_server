acd0d54478363fcefc64e82d98ec86890cfd28f5
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2013-01-26 22:36:58 +0100

    Added test and implemented simple initialization

--- src/sdd_player.erl
+++ src/sdd_player.erl
@@ -15,6 +15,12 @@
 %% API
 -export([handle_game_event/3]).
 
+%% Records
+-record(state, {
+	name,
+	secret
+}).
+
 %%% =================================================================================== %%%
 %%% API                                                                                 %%%
 %%% =================================================================================== %%%
@@ -25,3 +31,42 @@ handle_game_event(PlayerId, EventType, EventData) ->
 	catch
 		Error -> Error
 	end.
+
+%%% =================================================================================== %%%
+%%% GEN_SERVER CALLBACKS                                                                %%%
+%%% =================================================================================== %%%
+
+%% ------------------------------------------------------------------------------------- %%
+%% Creates a history for a new player, assigns the realizer function to it
+%% and adds a register event with the given player information
+
+init(PlayerInfo) ->
+	EmptyHistory = sdd_history:new(fun realize_event/3),
+	InitialHistory = sdd_history:append(EmptyHistory, register, PlayerInfo),
+	{ok, InitialHistory}.
+
+%%% =================================================================================== %%%
+%%% HISTORY CALLBACKS                                                                   %%%
+%%% =================================================================================== %%%
+
+realize_event(_EmptyState, register, {Name, Secret}) ->
+	#state{name = Name, secret = Secret}.
+
+%%% =================================================================================== %%%
+%%% TESTS                                                                               %%%
+%%% =================================================================================== %%%
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+init_createsNewPlayerWithNameAndSecret_test() ->
+	{ok, InitialHistory} = init({"Peter", "secret"}),
+	State = sdd_history:state(InitialHistory),
+	?assertEqual(State#state.name, "Peter"),
+	?assertEqual(State#state.secret, "secret"),
+
+	Past = sdd_history:past(InitialHistory),
+	?assertMatch([{_Time, register, {"Peter", "secret"}}], Past).
+
+-endif.
