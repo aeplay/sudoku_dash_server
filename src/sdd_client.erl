7c4a56bb171011a71c1f10d58f7b2f2f980aced5
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2013-03-26 18:34:25 +0100

    Added barebones sdd_client

---
+++ src/sdd_client.erl
@@ -0,0 +1,37 @@
+%%% =================================================================================== %%%
+%%% Sudoku Dash Game: Client Server                                                     %%%
+%%%                                                                                     %%%
+%%% Copyright 2012 Anselm Eickhoff                                                      %%%
+%%%                                                                                     %%%
+%%% This is a gen_server that handles one client session                                %%%
+%%% - collects messages from player and game                                            %%%
+%%% - encodes and sends them to connected bullet servers                                %%%
+%%% =================================================================================== %%%
+
+-module(sdd_client).
+
+%% Records
+-record(
+	current_player,
+	current_game,
+	current_client,
+	current_client_active,
+	messages_for_client = []
+}).
+
+%%% =================================================================================== %%%
+%%% GEN_SERVER CALLBACKS                                                                %%%
+%%% =================================================================================== %%%
+
+%%% =================================================================================== %%%
+%%% TESTS                                                                               %%%
+%%% =================================================================================== %%%
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("sdd_history_test_macros.hrl").
+
+
+
+-endif.
