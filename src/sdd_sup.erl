4e3756c44f7b289cf59b471b8d0435bdb504ab80
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2013-03-29 22:48:17 +0100

    added jsx to rebar dependencies and added a main supervisor

--- src/sdd_sup.erl
+++ src/sdd_sup.erl
@@ -1,3 +1,11 @@
+%%% =================================================================================== %%%
+%%% Sudoku Dash Game: Main Supervisor                                                   %%%
+%%%                                                                                     %%%
+%%% Copyright 2012 Anselm Eickhoff                                                      %%%
+%%%                                                                                     %%%
+%%% This is the supervisor for all other supervisors                                    %%%
+%%% =================================================================================== %%%
+
 -module(sdd_sup).
 
 -behaviour(supervisor).
@@ -8,19 +16,32 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
-
-%% ===================================================================
-%% API functions
-%% ===================================================================
+%%% =================================================================================== %%%
+%%% API                                                                                 %%%
+%%% =================================================================================== %%%
 
 start_link() ->
-	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
+%%% =================================================================================== %%%
+%%% SUPERVISOR CALLBACKS                                                                %%%
+%%% =================================================================================== %%%
 
 init([]) ->
-	{ok, { {one_for_one, 5, 10}, []} }.
+	ClientSupSpec = {
+		clients_sup,
+		{sdd_clients_sup, start_link, []},
+		permanent,
+		1000,
+		supervisor,
+		[sdd_clients_sup]
+	},
+	PlayerSupSpec = {
+		players_sup,
+		{sdd_players_sup, start_link, []},
+		permanent,
+		1000,
+		supervisor,
+		[sdd_players_sup]
+	},
+	{ok, {{one_for_one, 5, 10}, [ClientSupSpec, PlayerSupSpec]}}.
