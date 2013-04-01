b242282ed917baacb66ed76b94855c1f762146a0
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2013-04-01 02:09:55 +0200

    set db dir one up

--- src/sdd.erl
+++ src/sdd.erl
@@ -7,13 +7,13 @@ start() ->
 	ok = application:start(crypto),
 	ok = application:start(ranch),
 	ok = application:start(cowboy),
-	application:set_env(mnesia, dir, "./db"),
+	application:set_env(mnesia, dir, "../db"),
 	ok = application:start(mnesia),
 	application:start(sasl),
 	ok = application:start(sdd).
 
 setup_persistence() ->
-	application:set_env(mnesia, dir, "./db"),
+	application:set_env(mnesia, dir, "../db"),
 	mnesia:create_schema([node()]),
 	application:start(mnesia),
 	sdd_history:setup_persistence(player_history),
